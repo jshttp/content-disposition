4510360040c497aa73003f34a6c32c4e70a15379
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-09-20 09:48:01 -0700

    tests: wrap tests for shorter lines

--- test/test.js
+++ test/test.js
@@ -10,43 +10,53 @@ describe('contentDisposition()', function () {
 
 describe('contentDisposition(filename)', function () {
   it('should require a string', function () {
-    assert.throws(contentDisposition.bind(null, 42), /argument filename.*string/)
+    assert.throws(contentDisposition.bind(null, 42),
+      /argument filename.*string/)
   })
 
   it('should create a header with file name', function () {
-    assert.equal(contentDisposition('plans.pdf'), 'attachment; filename="plans.pdf"')
+    assert.equal(contentDisposition('plans.pdf'),
+      'attachment; filename="plans.pdf"')
   })
 
   it('should use the basename of the string', function () {
-    assert.equal(contentDisposition('/path/to/plans.pdf'), 'attachment; filename="plans.pdf"')
+    assert.equal(contentDisposition('/path/to/plans.pdf'),
+      'attachment; filename="plans.pdf"')
   })
 
   it('should not accept filename with NULLs', function () {
-    assert.throws(contentDisposition.bind(null, 'plans\u0000.pdf'), /invalid.*value/)
+    assert.throws(contentDisposition.bind(null, 'plans\u0000.pdf'),
+      /invalid.*value/)
   })
 
   describe('when "filename" is US-ASCII', function () {
     it('should only include filename parameter', function () {
-      assert.equal(contentDisposition('plans.pdf'), 'attachment; filename="plans.pdf"')
+      assert.equal(contentDisposition('plans.pdf'),
+        'attachment; filename="plans.pdf"')
     })
 
     it('should escape quotes', function () {
-      assert.equal(contentDisposition('the "plans".pdf'), 'attachment; filename="the \\"plans\\".pdf"')
+      assert.equal(contentDisposition('the "plans".pdf'),
+        'attachment; filename="the \\"plans\\".pdf"')
     })
   })
 
   describe('when "filename" is Unicode', function () {
     it('should include filename* parameter', function () {
-      assert.equal(contentDisposition('планы.pdf'), 'attachment; filename="?????.pdf"; filename*=UTF-8\'\'%D0%BF%D0%BB%D0%B0%D0%BD%D1%8B.pdf')
+      assert.equal(contentDisposition('планы.pdf'),
+        'attachment; filename="?????.pdf"; filename*=UTF-8\'\'%D0%BF%D0%BB%D0%B0%D0%BD%D1%8B.pdf')
     })
 
     it('should include filename fallback', function () {
-      assert.equal(contentDisposition('«bye».pdf'), 'attachment; filename="?bye?.pdf"; filename*=UTF-8\'\'%C2%ABbye%C2%BB.pdf')
-      assert.equal(contentDisposition('«hi».pdf'), 'attachment; filename="?hi?.pdf"; filename*=UTF-8\'\'%C2%ABhi%C2%BB.pdf')
+      assert.equal(contentDisposition('«bye».pdf'),
+        'attachment; filename="?bye?.pdf"; filename*=UTF-8\'\'%C2%ABbye%C2%BB.pdf')
+      assert.equal(contentDisposition('«hi».pdf'),
+        'attachment; filename="?hi?.pdf"; filename*=UTF-8\'\'%C2%ABhi%C2%BB.pdf')
     })
 
     it('should encode special characters', function () {
-      assert.equal(contentDisposition('«\'*%()».pdf'), 'attachment; filename="?\'*%()?.pdf"; filename*=UTF-8\'\'%C2%AB%27%2A%25%28%29%C2%BB.pdf')
+      assert.equal(contentDisposition('«\'*%()».pdf'),
+        'attachment; filename="?\'*%()?.pdf"; filename*=UTF-8\'\'%C2%AB%27%2A%25%28%29%C2%BB.pdf')
     })
   })
 
